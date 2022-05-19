a99ab323f8d6d8010027228d6eab9a794b6a28d9
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-19 15:04:46 +0600

    .env local file added

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -8,6 +8,13 @@ const firebaseConfig = {
     storageBucket: "posterisks.appspot.com",
     messagingSenderId: "684640806161",
     appId: "1:684640806161:web:4bb7d3dbb2c952b2c5d8b0"
+
+    /*apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID*/
 };
 
 const app = initializeApp(firebaseConfig);
