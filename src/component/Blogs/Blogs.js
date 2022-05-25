5b62e9851bcfa1dd9dfdf8bc078c505cfeca9c58
Tanmay Kumar <itstanmaykumar@gmail.com>
2022-05-26 04:27:55 +0600

    blogs added

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -6,55 +6,39 @@ const Blogs = () => {
             <div className="mx-auto px-3 pb-5 mx-3 my-5">
                 <h1 className="my-4 p-3 text-center">Explore Our Recent Blogs</h1>
                 <article className="p-5 rounded-10 shadow-lg">
-                    <h3 className="mb-3">Difference between Authentication and Authorization -</h3>
-                    <p>Authentication & Authorization are both used to maintain the safety of the resources of an automated system. Despite the similar-sounding terms, authentication and authorization are very different process with respect to each other. Authentication validates if the users are whom they claim to be. On the other hand, Authorization gives permission to access the resourses of the system. Authentication identifies the user, but authorization identifies the permission of accessibility of that user. Authentication is done before authorization. Authorization works through passwords & credentials, but authentication works through settings maintained by security teams.</p>
-                </article>
-                <article className="p-5 my-5 rounded-10 shadow-lg">
-                    <h3 className="mb-3">Why are you using firebase? What other options do you have to implement authentication?</h3>
-                    <p>I using Firebase to host my website and to authenticate my website.</p>
-                    <p>Firebase is a traditional Social Authentication method. It's is widely popular now-a-days. Though there exist some other methods of authentication. Such as -</p>
+                    <h3 className="mb-3">Difference between Javascript and Node.js -</h3>
                     <ul>
-                        <li>
-                            <h5>Token Authentication</h5>
-                            <p>In this method, a digitally encoded signature is used to authenticate and authorize the correct user.</p>
-                        </li>
-                        <li className='my-3'>
-                            <h5>Standard Authentication</h5>
-                            <p>This method helps users authenticate by entering their credentials using a user id and a strong password.</p>
-                        </li>
-                        <li className='my-3'>
-                            <h5>Multi-Factor Authentication (MFA)</h5>
-                            <p>It is a multi-layered authentication method which examines the identity of users for login or other access permission.</p>
-                        </li>
-                        <li >
-                            <h5>Passwordless Authentication</h5>
-                            <p>In this method, a magic link, fingerprint, PIN, or a secret token delivered via email or text message is used to authenticate and authorize the correct user.</p>
-                        </li>
+                        <li>Javascript is a programming language, but Node.js is not a programming language.</li>
+                        <li>Javascript is used for writing scripts for websites and NodeJS is a  runtime environment for Javascript.</li>
+                        <li>Javacript cannot be run outside the browser without the help of node.js.</li>
+                        <li>Javascript is mainly used for client-side and node.js is mostly used for server-side</li>
+                        <li>Javascript can add HTML, but node.js can't add HTML.</li>
                     </ul>
                 </article>
                 <article className="p-5 my-5 rounded-10 shadow-lg">
-                    <h3 className="mb-3">What other services does firebase provide other than authentication?</h3>
-                    <div>
-                        <p>There are many services which Firebase provides, Most useful of them is Authentication. But there are also some useful services that help you to develop high-quality mobile and web applications. Such as -</p>
-                        <ul>
-                            <li>
-                                <h5>Cloud Messaging</h5>
-                                <p>Firebase can implement notifications, and it is fast, reliable, and scalable.</p>
-                            </li>
-                            <li className='my-3'>
-                                <h5>Dynamic Links</h5>
-                                <p>Firebase can generate deep links which redirect user to your applications.</p>
-                            </li>
-                            <li className='my-3'>
-                                <h5>Predictions</h5>
-                                <p>Firebase can help in making predictions about your application decisions with the help of google's AI.</p>
-                            </li>
-                            <li >
-                                <h5>Google Analytics</h5>
-                                <p>Firebase can help in growing and managing your application. It can become helpful in tracking user demographics, behavior, purchasing patterns, revenue reports.</p>
-                            </li>
-                        </ul>
-                    </div>
+                    <h3 className="mb-3">Difference between SQL and NoSQL -</h3>
+                    <ul>
+                        <li>SQL is used for relational databases, but NoSQL is used for non-relational database.</li>
+                        <li>SQL uses fixed or static or predefined schema, but NoSQL uses dynamic schema.</li>
+                        <li>SQL cannot be used for hierarchical data storage, but NoSQL is best for hierarchical data storage.</li>
+                        <li>SQL is best suited for complex queries and NoSQL is not so good best suited for complex queries.</li>
+                        <li>SQL databases are vertically scalable, but NoSQL databases are horizontally scalable.</li>
+                        <li>SQL databases are are table based, but NoSQL databases are are document, key-value, graph or wide-column stores.</li>
+                    </ul>
+                </article>
+                <article className="p-5 my-5 rounded-10 shadow-lg">
+                    <h3 className="mb-3">What is the purpose of JWT and how it works?</h3>
+                    <h5>Purpose -</h5>
+                    <p>JWT means JSON Web Token. It is an open standard used to share security information between two parties (a client and a server). It is a very popular standard which is used to trust and verify requests by using digital signatures, and to exchange information between parties.</p>
+                    <h5>Working Procedure -</h5>
+                    <p>A JWT is a string made up of three parts (header, payload, signature), separated by dots (.). Therefore, a JWT typically looks like the following. <br /><b>xxxxx.yyyyy.zzzzz</b></p>
+                    <ul>
+                        <li><b>header</b> contains contains the type of secret token and the signing algorithm.</li>
+                        <li><b>payload</b> contains contains the claims.</li>
+                        <li><b>signature</b> ensures that the token hasn’t been altered.</li>
+                    </ul>
+                    <p>When a user logs in or registers, the authentication server verifies the credentials and issues a jwt signed using a secret key. Then the resource server verifies the authenticity of the token using the secret key. It uses the signature to verify the information wasn't changed along the way.</p>
+
                 </article>
             </div>
         </section>
